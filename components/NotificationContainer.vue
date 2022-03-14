ddbcb42a809d64cd2ca00aad5dd6a6c8bf9f5309
AxelRSa <axelg.rizos@gmail.com>
2022-03-14 10:17:29 -0500

    adding notification components and make it work

---
+++ components/NotificationContainer.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="notification-container">
+    <NotificationBar
+      v-for="notification in notifications"
+      :key="notification.id"
+      :notification="notification"
+    />
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import NotificationBar from '@/components/NotificationBar.vue'
+export default {
+  components: {
+    NotificationBar
+  },
+  computed: mapState('notification', ['notifications'])
+}
+</script>
+
+<style scoped>
+.notification-container {
+  position: fixed;
+  top: 80px;
+  left: 4rem;
+  z-index: 2147483649;
+}
+</style>
